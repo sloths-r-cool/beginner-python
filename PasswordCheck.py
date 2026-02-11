bbc6ce905840317752ba3a2110fabc04256c8f4d
sloths-r-cool <enzokadiu@gmail.com>
2026-02-11 21:01:48 +0000

    Update PasswordCheck.py

--- PasswordCheck.py
+++ PasswordCheck.py
@@ -2,13 +2,15 @@ password = input("Enter a password:")
 
 has_number = False
 has_upper = False
+# So bascially i interpreted these values as false so that when the for loop runs it chnages these values to True
+
 
 for char in password:
     if char.isdigit():
         has_number = True
     if char.isupper():
         has_upper = True
-       
+# These set the values to True/False for the next lines of code that check the password       
 if len(password) < 8:
     print("Password too short, must be over 8 characters.")
 elif not has_number:
@@ -17,3 +19,4 @@ elif not has_upper:
     print("Add an uppercase letter.")
 else:
     print("Password is strong enough")
+    # Thanks
